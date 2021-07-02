53a544a960197c1a08d972336161d5436fcaa168
Prajwal Shah <prajwalshah1721@gmail.com>
2021-07-03 00:26:39 +0530

    Added styles and other changes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import { createBrowserHistory } from "history";
+import { useHistory } from "react-router-dom";
+// import { createBrowserHistory } from 'history';
 import React, { useState, useEffect } from 'react';
 import { UrlPage } from './pages/UrlPage/UrlPage';
+// import about{ CreateLink } from './components/createLink/createLink';
 // import { QR } from './components/Overlays/QR/qr';
 import { Navbar } from './components/Navbar/navbar';
 import { Signin } from './components/Auth/Signin/signin';
@@ -9,12 +11,16 @@ import { SignUp } from './components/Auth/Signup/signup';
 import { RedirectLink } from './components/Redirect/redirectLink';
 import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import axios from './axiosurl';
-let history = createBrowserHistory();
+import { NotLogged } from './components/NotLogged/notLogged';
 const App = () => {
   // check for authentication first if logged in and all stuff
   const [user, setUser] = useState(null);
   const [urlLeft, setUrlLeft] = useState(null);
+  const [view, toggleView] = useState(true);
+  const history = useHistory();
+  // const history = createBrowserHistory();
   useEffect(() => {
+    // console.log(user);
     axios.get('/')
       .then(
         (res) => {
@@ -25,29 +31,29 @@ const App = () => {
       .catch((err) => {
         console.log(err);
       })
-  }, [user]);
+  }, [user, urlLeft]);
   return (
     <Router history={history}>
       <div className="App">
-        <Navbar user={user} used={urlLeft} />
-        <Signin />
-        <Switch>
-          {!user ?
-            <>
-              <Route exact path="/signup" component={SignUp} />
-              <Route path='login' component={Signin} />
-              <Redirect path='login' component={Signin} />
-            </> :
-            <>
-              <Route path="/:user/:shortUrl" component={RedirectLink} />
-              <Route path="/" component={UrlPage} />
-              <Redirect path='/' component={UrlPage} />
-
-            </>
-          }
-        </Switch>
+        {/* {!user && history.location.pathname === '/' ? <NotLogged toggleView={toggleView} /> : <></>} */}
+        {!user ? <></> : <Navbar user={user} used={urlLeft} />}
+        {
+          !user ?
+            <Switch>
+              <Route exact path="/signup" ><SignUp /></Route >
+              <Route   ><Signin /></Route>
+              {/* <Redirect from='/' to='/login' /> */}
+            </Switch> :
+            <Switch>
+              <Route exact path="/:user/:shortUrl" component={RedirectLink} />
+              <Route exact path='/'  >
+                <UrlPage user={user} />
+              </Route>
+              <Redirect to='/' />
+            </Switch>
+        }
         {/* page under construction */}
-      </div>
+      </div >
     </Router >
   );
 }
